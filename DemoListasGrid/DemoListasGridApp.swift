c6c31ac6c84bf0b36e74086cd9548c8355464fc3
cesar <cursos.mapatupisaro@gamail.com>
2024-02-16 19:54:43 -0600

    Initial Commit

---
+++ DemoListasGrid/DemoListasGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoListasGridApp.swift
+//  DemoListasGrid
+//
+//  Created by Aguid Ramirez Sanchez on 16/02/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoListasGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
